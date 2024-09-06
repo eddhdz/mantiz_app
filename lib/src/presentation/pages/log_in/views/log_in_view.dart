44f5dcfef863e72b811b51d739f5d47424891477
Eduardo Hernandez <eduardohernandez@MacBook-Air-de-Eduardo.local>
2024-09-06 15:22:36 -0600

    refactor: cambio en la logica del loginview para separarla

--- lib/src/presentation/pages/log_in/views/log_in_view.dart
+++ lib/src/presentation/pages/log_in/views/log_in_view.dart
@@ -1,171 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:mantiz/src/presentation/global/widgets/buttons/submit_button.dart';
 
-import '../../../../domain/enums.dart';
-import '../../../../domain/repositories/authentication_repository.dart';
 import '../../../global/colors.dart';
-import '../../../global/widgets/buttons/general_button.dart';
-import '../../../routes/routes.dart';
+import '../controller/log_in_controller.dart';
 
 import 'package:provider/provider.dart';
 
-class LogInView extends StatefulWidget {
+class LogInView extends StatelessWidget {
   const LogInView({super.key});
 
   @override
-  State<LogInView> createState() => _LogInViewState();
-}
-
-class _LogInViewState extends State<LogInView> {
-  String _userName = '', _password = '';
-  bool _fetching = false;
-  @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              colors: [blueLightGlobalColor, blueStrongGlobalColor],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            )),
-          ),
-          SafeArea(
-            child: Center(
-              child: Container(
-                width: size.width * 0.9,
-                height: size.height * 0.6,
-                decoration: BoxDecoration(
-                    color: whiteGlobalColor,
-                    borderRadius: BorderRadius.circular(15)),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Form(
-                    child: AbsorbPointer(
-                      absorbing: _fetching,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const Text(
-                            'Login',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          TextFormField(
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            decoration: const InputDecoration(
-                                label: Text(
-                                  'Nombre de usuario',
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                                floatingLabelBehavior:
-                                    FloatingLabelBehavior.always,
-                                filled: true,
-                                fillColor: blueExtraLightGlobalColor,
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide.none,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(10)))),
-                            onChanged: (text) {
-                              _userName = text.trim().toLowerCase();
-                            },
-                            validator: (value) {
-                              value = value?.trim().toLowerCase() ?? '';
-                              if (value.isEmpty) {
-                                return 'Invalid Username';
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              obscureText: true,
-                              decoration: const InputDecoration(
-                                  label: Text('Contraseña',
+
+    return ChangeNotifierProvider<LogInController>(
+      create: (_) => LogInController(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [blueLightGlobalColor, blueStrongGlobalColor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )),
+            ),
+            SafeArea(
+              child: Center(
+                child: Container(
+                  width: size.width * 0.9,
+                  height: size.height * 0.6,
+                  decoration: BoxDecoration(
+                      color: whiteGlobalColor,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Form(
+                      child: Builder(builder: (context) {
+                        final controller =
+                            Provider.of<LogInController>(context);
+                        return AbsorbPointer(
+                          absorbing: controller.fetching,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              TextFormField(
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                decoration: const InputDecoration(
+                                    label: Text(
+                                      'Nombre de usuario',
                                       style: TextStyle(
-                                          fontWeight: FontWeight.w600)),
-                                  floatingLabelBehavior:
-                                      FloatingLabelBehavior.always,
-                                  filled: true,
-                                  fillColor: blueExtraLightGlobalColor,
-                                  border: OutlineInputBorder(
-                                      borderSide: BorderSide.none,
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(10)))),
-                              onChanged: (text) {
-                                _password = text.replaceAll(' ', '');
-                              },
-                              validator: (value) {
-                                value = value?.replaceAll(' ', '') ?? '';
-                                if (value.length < 3) {
-                                  return 'Invalid Password';
-                                }
-                                return null;
-                              }),
-                          Builder(builder: (context) {
-                            if (_fetching) {
-                              return const CircularProgressIndicator();
-                            }
-                            return GeneralButton(
-                                text: 'Iniciar sesión',
-                                onPressed: () {
-                                  final isValid = Form.of(context).validate();
-                                  if (isValid) {
-                                    _submit(context);
-                                  }
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    floatingLabelBehavior:
+                                        FloatingLabelBehavior.always,
+                                    filled: true,
+                                    fillColor: blueExtraLightGlobalColor,
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide.none,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)))),
+                                onChanged: (text) {
+                                  controller.onUserNameChanged(text);
                                 },
-                                color: blueNeutralGlobalColor,
-                                textColor: whiteGlobalColor);
-                          })
-                        ],
-                      ),
+                                validator: (value) {
+                                  value = value?.trim().toLowerCase() ?? '';
+                                  if (value.isEmpty) {
+                                    return 'Invalid Username';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              TextFormField(
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  obscureText: true,
+                                  decoration: const InputDecoration(
+                                      label: Text('Contraseña',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600)),
+                                      floatingLabelBehavior:
+                                          FloatingLabelBehavior.always,
+                                      filled: true,
+                                      fillColor: blueExtraLightGlobalColor,
+                                      border: OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(10)))),
+                                  onChanged: (text) {
+                                    controller.onPasswordChanged(text);
+                                  },
+                                  validator: (value) {
+                                    value = value?.replaceAll(' ', '') ?? '';
+                                    if (value.length < 3) {
+                                      return 'Invalid Password';
+                                    }
+                                    return null;
+                                  }),
+                              const SubmitButton()
+                            ],
+                          ),
+                        );
+                      }),
                     ),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    );
-  }
-
-  Future<void> _submit(BuildContext context) async {
-    setState(() {
-      _fetching = true;
-    });
-    final result =
-        await Provider.of<AuthenticationRepository>(context, listen: false)
-            .signIn(_userName, _password);
-
-    if (!mounted) {
-      return;
-    }
-
-    result.when(
-      (failure) {
-        setState(() {
-          _fetching = false;
-        });
-        final message = {
-          SignInFailure.notFound: 'Not Found',
-          SignInFailure.unauthorized: 'Invalid credentials',
-          SignInFailure.unknown: 'Error',
-          SignInFailure.network: 'No internet'
-        }[failure];
-
-        ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text(message!)));
-      },
-      (userInfo) {
-        Navigator.pushReplacementNamed(context, Routes.home);
-      },
     );
   }
 }
