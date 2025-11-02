cf4e1bcaeb0f480c09daa480d6e90abb9b249feb
EdgarSalcido <edgarsalcidomundo@gmail.com>
2025-11-01 21:58:22 -0600

    Card de ticket completa

--- lib/src/presentation/pages/third_page/view/third_page_view.dart
+++ lib/src/presentation/pages/third_page/view/third_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mantiz/src/presentation/global/widgets/customs/custom_asign_to.dart';
 
 import '../../../../data/models/ticket_model.dart';
 import '../../../global/colors.dart';
@@ -82,6 +83,111 @@ class ThirdPageView extends StatelessWidget {
                               const SizedBox(width: 5),
                             ]))
                       ]),
+
+                      //! Título ...
+                      Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: Row(children: <Widget>[
+                            const SizedBox(width: 5),
+                            const GeneralText(
+                                mensaje: 'Título:', maxLines: 1, overFlow: TextOverflow.ellipsis, size: 14, weight: FontWeight.bold, color: blackPanter, align: TextAlign.left),
+                            Expanded(
+                                child: GeneralText(
+                                    mensaje: tickets[index].title!,
+                                    maxLines: 1,
+                                    overFlow: TextOverflow.ellipsis,
+                                    size: 14,
+                                    weight: FontWeight.normal,
+                                    color: blackPanter,
+                                    align: TextAlign.left)),
+                            const SizedBox(width: 5),
+                          ])),
+
+                      Row(children: [
+                        //! Type ...
+                        Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(children: <Widget>[
+                              const SizedBox(width: 5),
+                              const GeneralText(
+                                  mensaje: 'Tipo:', maxLines: 1, overFlow: TextOverflow.ellipsis, size: 14, weight: FontWeight.bold, color: blackPanter, align: TextAlign.left),
+                              GeneralText(
+                                  mensaje: tickets[index].type,
+                                  maxLines: 1,
+                                  overFlow: TextOverflow.ellipsis,
+                                  size: 14,
+                                  weight: FontWeight.normal,
+                                  color: blackPanter,
+                                  align: TextAlign.left),
+                              const SizedBox(width: 5),
+                            ])),
+
+                        Expanded(child: Container()),
+                        //! Area ...
+                        Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(children: <Widget>[
+                              const SizedBox(width: 5),
+                              const GeneralText(
+                                  mensaje: 'Area:', maxLines: 1, overFlow: TextOverflow.ellipsis, size: 14, weight: FontWeight.bold, color: blackPanter, align: TextAlign.left),
+                              GeneralText(
+                                  mensaje: tickets[index].area,
+                                  maxLines: 1,
+                                  overFlow: TextOverflow.ellipsis,
+                                  size: 14,
+                                  weight: FontWeight.normal,
+                                  color: blackPanter,
+                                  align: TextAlign.right),
+                              const SizedBox(width: 5),
+                            ]))
+                      ]),
+
+                      Row(children: [
+                        //! Estatus ...
+                        Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(children: <Widget>[
+                              const SizedBox(width: 5),
+                              const GeneralText(
+                                  mensaje: 'Estatus:', maxLines: 1, overFlow: TextOverflow.ellipsis, size: 14, weight: FontWeight.bold, color: blackPanter, align: TextAlign.left),
+                              GeneralText(
+                                  mensaje: tickets[index].status,
+                                  maxLines: 1,
+                                  overFlow: TextOverflow.ellipsis,
+                                  size: 14,
+                                  weight: FontWeight.bold,
+                                  color: (tickets[index].status.toLowerCase() == 'actualizado' ||
+                                          tickets[index].status.toLowerCase() == 'abierto' ||
+                                          tickets[index].status.toLowerCase() == 'asignado' ||
+                                          tickets[index].status.toLowerCase() == 'aprobado' ||
+                                          tickets[index].status.toLowerCase() == 'agendado')
+                                      ? orangePrincipal
+                                      : (tickets[index].status.toLowerCase() == 'suspendido' ||
+                                              tickets[index].status.toLowerCase() == 'cancelado' ||
+                                              tickets[index].status.toLowerCase() == 'rechazado')
+                                          ? redPrincipal
+                                          : (tickets[index].status.toLowerCase() == 'rechazado' || tickets[index].status.toLowerCase() == 'creado')
+                                              ? blueLightGlobalColor
+                                              : (tickets[index].status.toLowerCase() == 'finalizado')
+                                                  ? greenPrincipal
+                                                  : blackPanter,
+                                  align: TextAlign.left),
+                              const SizedBox(width: 5),
+                            ])),
+
+                        Expanded(child: Container()),
+
+                        IconButton(
+                          onPressed: () async {
+                            await showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return CustomAsignTo(user: tickets[index].attendance.asignedto);
+                                });
+                          },
+                          icon: const Icon(Icons.account_circle_outlined, size: 35),
+                        )
+                      ])
                     ]),
                   ));
             },
